3b8ac05cf3c7c51c011a9d3c389c5450d55fd764
Huy Nguyen <giahuy2201@icloud.com>
2019-08-01 11:26:07 -0400

    xong phan khoi tao va in

--- liet-ke-chinh-hop/chinhHop.java
+++ liet-ke-chinh-hop/chinhHop.java
@@ -10,6 +10,27 @@ public class chinhHop {
         // n & k
         int n = 6, k = 4;
 
-        
+        inChinhHop(6,4);
+    }
+
+    public static void inChinhHop(int n, int k){
+        // tao ra mang
+        int[] chinhHop = new int[k];
+        khoiTao(chinhHop);
+
+        inRa(chinhHop);
+    }
+
+    public static void khoiTao(int[] mang){
+        for(int i=0; i< mang.length ; i++){
+            mang[i] = 1;
+        }
+    }
+
+    public static void inRa(int[] mang){
+        for(int i=0; i< mang.length ; i++) {
+            System.out.print(mang[i] + " ");
+        }
+        System.out.println();
     }
 }
