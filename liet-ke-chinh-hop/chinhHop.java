b59337363529a2d6413104696d2219d5f28f8bae
Huy Nguyen <giahuy2201@icloud.com>
2019-08-01 11:51:02 -0400

    xong chinh hop lap co ban

--- liet-ke-chinh-hop/chinhHop.java
+++ liet-ke-chinh-hop/chinhHop.java
@@ -1,6 +1,6 @@
 
 /* 
- * Code in ra chỉnh hợp không lặp từ 2 số n và k cho sẵn 
+ * Code in ra chỉnh hợp lặp từ 2 số n và k cho sẵn 
  */
 
 public class chinhHop {
@@ -18,7 +18,27 @@ public class chinhHop {
         int[] chinhHop = new int[k];
         khoiTao(chinhHop);
 
-        inRa(chinhHop);
+        // bien kiem tra  
+        int i = k-1;
+
+        while(i >= 0) {
+            i=k-1;
+            inRa(chinhHop);
+            // tai tao cau hinh chinh hop
+
+            while(i >= 0 && chinhHop[i] == n){
+                i--; // xem xet so phia truoc
+            }
+            if (i >= 0 && chinhHop[i] < n){
+                chinhHop[i]++;
+
+                for(int j=i+1;j<k;j++){
+                    chinhHop[j]=1;
+                }
+            }
+        }
+
+        // inRa(chinhHop);
     }
 
     public static void khoiTao(int[] mang){
