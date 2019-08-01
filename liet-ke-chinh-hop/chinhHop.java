3fb02bc2f990a180b963cfb37b5a22770cbc6fe9
Huy Nguyen <giahuy2201@icloud.com>
2019-08-01 11:16:48 -0400

    xong phan mo dau

---
+++ liet-ke-chinh-hop/chinhHop.java
@@ -0,0 +1,15 @@
+
+/* 
+ * Code in ra chỉnh hợp không lặp từ 2 số n và k cho sẵn 
+ */
+
+public class chinhHop {
+    
+    public static void main(String[] args){
+        
+        // n & k
+        int n = 6, k = 4;
+
+        
+    }
+}
