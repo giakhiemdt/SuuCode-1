1f5b5270892dfe232025b77e4989bf2236f0d25b
khiem1371 <khiem1371@gmail.com>
2019-09-08 23:01:44 +0700

    da xong phan nhi phan

---
+++ dayNhiPhanN/luyenTap/onTap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class onTap {
+    public static void main (String[] agrs) {
+        int a = 7;
+        int y ;
+        int x = 1;
+        for( ; x != 0 ;  ) {
+            x = a / 2 ;
+            y = a % 2 ;
+            System.out.print(y);
+        }
+
+        // System.out.println(a/2);
+    }
+}
